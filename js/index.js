af9167070f2640fe245c43b08bf2111051b1d20e
RPB <sumitomo.andi@gmail.com>
2023-06-28 15:58:38 +0700

    add header content

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
+function burger(x) {
+    x.classList.toggle("change");
+}
+
 function dont() {
     if (document.getElementById('ugly').checked) {
         alert("No, You are Ugly!!");}
         document.getElementById("ugly").checked = false;
-    }
-
-
-
+}
 
 function img(x){
     const y = document.getElementById("bite")
-    const z = document.getElementById("yay")
     if(x==0)
         y.style.display = "block";
     else
